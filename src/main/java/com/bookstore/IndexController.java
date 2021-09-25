e7c1bd8a63cd4e079494b2b36ffa6d404ee493d0
Georges034302 <georges.boughantous@gmail.com>
2021-09-26 04:20:04 +1000

    mysql config

--- src/main/java/com/bookstore/IndexController.java
+++ src/main/java/com/bookstore/IndexController.java
@@ -8,27 +8,29 @@ public class IndexController {
     @GetMapping("/")
     public String index() {
         StringBuilder sb = new StringBuilder();
-        sb.append("<!DOCTYPE html>"+
-        "<html>"+        
-                "<body>" +
-            "<head>"+
-            "<meta name='viewport' content='width=device-width, initial-scale=1'>"+
-            "<style>"+
-            "body, html {"+
-              "height: 100%;"+
-              "margin: 0;"+
-            "}"+            
-            ".bg {"+             
-              "background-image: url('https://i.morioh.com/dd054f30db.png');"+              
-              "height:100%;"+
-              "background-position: center;"+
-              "background-repeat: no-repeat;"+
-              "background-size: cover;"+
-            "}"+
-            "</style>"+
-            "</head>"+
+        sb.append(
+            "<!DOCTYPE html>"+
+                "<html>" +
+                "<head>"+
+                "<meta name='viewport' content='width=device-width, initial-scale=1'>"+
+                "<style>"+
+                "body, html {"+
+                  "height: 100%;"+
+                "margin: 0;" +
+                  "color:#ffffff"+
+                "}"+            
+                ".bg {"+             
+                  "background-image: url('https://i.morioh.com/dd054f30db.png');"+              
+                  "height:100%;"+
+                  "background-position: center;"+
+                  "background-repeat: no-repeat;"+
+                  "background-size: cover;"+
+                "}"+
+                "</style>"+
+                "</head>"+      
+            "<body>" +        
+                "<div class='bg'>" +
                 "<h1>Welcome to Bookstore Spring-boot API</h1>" +
-                "<div class='bg'>"+
                 "<ul>"+
                     "<li>Fetch all books: https://bookstore-springboot-backend.herokuapp.com/book/getAll</li>"+
                     "<li>Create book (GET): https://bookstore-springboot-backend.herokuapp.com/book/add {book}</li>"+
