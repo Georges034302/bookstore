1cc75ebef366402c22ba473d5db7c446ee3c7ab9
Georges034302 <georges.boughantous@gmail.com>
2021-09-26 03:54:29 +1000

    mysql config

--- src/main/java/com/bookstore/IndexController.java
+++ src/main/java/com/bookstore/IndexController.java
@@ -7,6 +7,22 @@ import org.springframework.web.bind.annotation.RestController;
 public class IndexController {
     @GetMapping("/")
     public String index() {
-        return "Hello there! I'm running.";
+        StringBuilder sb = new StringBuilder();
+        sb.append("<!DOCTYPE html>"+
+        "<html>"+
+            "<body>"+
+                "<h1>Welcome to Bookstore Spring-boot API</h1>"+
+                "<ul>"+
+                    "<li>Fetch all books: https://bookstore-springboot-backend.herokuapp.com/book/getAll</li>"+
+                    "<li>Create book (GET): https://bookstore-springboot-backend.herokuapp.com/book/add {book}</li>"+
+                    "<li>Read book (GET): https://bookstore-springboot-backend.herokuapp.com/book/{id}</li>"+
+                    "<li>Update book (PUT): https://bookstore-springboot-backend.herokuapp.com/book/{id}</li>"+
+                    "<li>Delete book (DELETE): https://bookstore-springboot-backend.herokuapp.com/book/{id}</li>"+
+                    "<li>Update book rating (PUT): https://bookstore-springboot-backend.herokuapp.com/book/rating/ {book} {id}</li>"+
+                    "<li>Update book rating (PUT): https://bookstore-springboot-backend.herokuapp.com/book/status/ {book} {id}</li>"+
+                "</ul>"+
+            "</body>"+       
+        "</html>");
+        return sb.toString();
     }
 }
